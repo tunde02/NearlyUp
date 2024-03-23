6a998704ad7c66cec81c96a7dc28b9b03e8c0a48
Seongho Jin <snskrktja@naver.com>
2024-03-24 04:57:17 +0900

    Modify player's movement to maintain movement direction consistently during camera rotation

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,9 +6,10 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float movePower = 35f;
-    public float jumpPower = 2500f;
-    public float velocityLimit = 5f;
+    public float movePower = 75f;
+    public float jumpPower = 3000f;
+    public float velocityLimit = 20f;
+    public Transform cameraArm;
 
 
     private Rigidbody rb;
@@ -32,9 +33,18 @@ public class PlayerController : MonoBehaviour
         // 제한 속도에 대한 플레이어의 현재 속도 비율을 movePower에 곱하여
         // 현재 속도가 빠를수록 이동할 때 주는 힘이 작아지도록 설정
         float power = movePower * GetVelocityRatio();
+        
+        // 카메라가 회전해도 앞, 뒤, 왼쪽, 오른쪽 조작이 동일하도록
+        // 카메라의 회전값을 힘의 방향에 더해줌
+        Vector3 cameraForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
+        Vector3 cameraRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
+        
+        Vector3 force = cameraRight * moveAmount.x + cameraForward * moveAmount.y;
+        Vector3 torque = cameraRight * moveAmount.y - cameraForward * moveAmount.x;
+        force.y = -0.05f;
 
-        rb.AddForce(new Vector3(moveAmount.x, -0.05f, moveAmount.y) * power);
-        rb.AddTorque(new Vector3(moveAmount.y, 0, -moveAmount.x) * power);
+        rb.AddForce(force * power);
+        rb.AddTorque(torque * power);
     }
 
     /// <summary>
